a6ff9357af5051c7a241536377935b3d32e04bc7
Jônatas <jonatasthome04@gmail.com>
2025-03-08 23:21:48 -0300

    Project Created

---
+++ src/test/java/com/devsuperior2/exercicio20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior2.exercicio20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
